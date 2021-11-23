a61fd9a8c80bf769218cd2f3cb7f446a41294366
Morgan Gardner <morgan.gardner2112@gmail.com>
2021-11-23 19:08:26 +0000

    Added properties and getters to RatGameProperties.java Needs the classes for ItemGenerator and Leaderboard (worked on in leaderboard branch).

--- src/game/RatGameProperties.java
+++ src/game/RatGameProperties.java
@@ -1,7 +1,46 @@
 package game;
 
+import game.event.GameActionListener;
+import game.player.Player;
+
+/**
+ * Rat Game Properties gives access to the properties of the game. This
+ * includes the player, the item generator, the action listener and the
+ * leaderboard.
+ *
+ * @author Morgan Gardner
+ * @version 0.1
+ * Copyright: N/A
+ */
 public class RatGameProperties {
 
+    //private ItemGenerator itemGenerator;
+    private GameActionListener actionListener;
+    private Player player;
+    //private Leaderboard leaderboard;
+
+    public RatGameProperties(GameActionListener actionListener, Player player) {
+        this.actionListener = actionListener;
+        this.player = player;
+    }
 
 
+
+    public GameActionListener getActionListener() {
+        return actionListener;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    /*public ItemGenerator getItemGenerator() {
+        return itemGenerator;
+    }
+    */
+
+    /*public Leaderboard getLeaderboard() {
+        return leaderboard;
+    }
+     */
 }
