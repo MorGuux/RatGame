9f9f39937ff31931aefd086a9156bbcf12da4747
Morgan Gardner <morgan.gardner2112@gmail.com>
2021-11-23 20:32:05 +0000

    Fixed broken Javadoc
    Accident while fixing conflicts

--- src/game/RatGame.java
+++ src/game/RatGame.java
@@ -81,6 +81,7 @@ public class RatGame {
     /**
      * Gets the currently active player.
      * @return The currently active player.
+     */
     public Player getPlayer() {
         return properties.getPlayer();
     }
