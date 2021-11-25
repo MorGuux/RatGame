981d96ad631f85c3da1890f073a74ab65d59832d
Ryan Payne <ryanpayne0511@gmail.com>
2021-11-25 23:34:26 +0000

    Commented & cleanup.

--- src/game/RatGameProperties.java
+++ src/game/RatGameProperties.java
@@ -15,26 +15,66 @@ import game.player.Player;
  */
 public class RatGameProperties {
 
-    private RatItemGenerator itemGenerator;
-    private GameActionListener actionListener;
-    private final int maxHostileEntities;
-    private Player player;
-    //private Leaderboard leaderboard;
+    //todo Leaderboard is currently Object. Replace with proper leaderboard.
 
+    /**
+     * Item pool generator setup ready to be used for the game.
+     */
+    private final RatItemGenerator itemGenerator;
+
+    /**
+     * Event handler target which will take rat game events.
+     */
+    private final GameActionListener actionListener;
+
+    /**
+     * Maximum number of hostile entities allowed before the game ends.
+     */
+    private final int maxHostileEntities;
+
+    /**
+     * The player whose playing the rat game.
+     */
+    private final Player player;
+
+    /**
+     * Leaderboard for all the players who have played the Rat game.
+     */
+    private final Object leaderboard;
+
+
+    /**
+     * @param eventHandler          Handler that takes a game event.
+     * @param itemGenerator         Item generator handler that handles the
+     *                              generation of game events.
+     * @param maxHostileEntityCount Maximum number of hostile entities before
+     *                              the game ends in a loss.
+     * @param leaderboard           Leaderboard of players who've completed
+     *                              the level being played.
+     * @param player                Player who is playing the game.
+     */
     public RatGameProperties(final GameActionListener eventHandler,
                              final RatItemGenerator itemGenerator,
                              final int maxHostileEntityCount,
+                             final Object leaderboard,
                              final Player player) {
         this.actionListener = eventHandler;
         this.itemGenerator = itemGenerator;
         this.maxHostileEntities = maxHostileEntityCount;
         this.player = player;
+        this.leaderboard = leaderboard;
     }
 
+    /**
+     * @return Event handler.
+     */
     public GameActionListener getActionListener() {
         return actionListener;
     }
 
+    /**
+     * @return Current player.
+     */
     public Player getPlayer() {
         return player;
     }
@@ -46,13 +86,17 @@ public class RatGameProperties {
         return this.maxHostileEntities;
     }
 
-    /*public ItemGenerator getItemGenerator() {
+    /**
+     * @return Item generator for the level.
+     */
+    public RatItemGenerator getItemGenerator() {
         return itemGenerator;
     }
-    */
 
-    /*public Leaderboard getLeaderboard() {
+    /**
+     * @return Leaderboard of players.
+     */
+    public Object getLeaderboard() {
         return leaderboard;
     }
-     */
 }
