95493a30a1f60935f4408ff27364658cf9a45622
Morgan Gardner <morgan.gardner2112@gmail.com>
2021-11-28 16:07:11 +0000

    Changed Leaderboard class to use given players and active player

--- src/game/player/leaderboard/Leaderboard.java
+++ src/game/player/leaderboard/Leaderboard.java
@@ -1,19 +1,24 @@
 package game.player.leaderboard;
 
+import game.player.Player;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Leaderboard {
 
-    private ArrayList<LeaderboardEntry> players;
-    private final int targetLevel;
+    private ArrayList<LeaderboardEntry> entries;
+    private List<Player> players;
+    private Player activePlayer;
 
     /**
      * Create a new leaderboard with a given target level. The entries will
      * be read from the associated level file.
-     * @param targetLevel the target level
+     * @param players The players to be added to the leaderboard.
      */
-    public Leaderboard(int targetLevel) {
-        this.targetLevel = targetLevel;
+    public Leaderboard(List<Player> players, Player activePlayer) {
+        this.players = players;
+        this.activePlayer = activePlayer;
     }
 
 }
