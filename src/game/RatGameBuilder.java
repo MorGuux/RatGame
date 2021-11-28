06bf256eff5192663889b2bb098d0d6493f27b37
Morgan Gardner <morgan.gardner2112@gmail.com>
2021-11-28 16:07:38 +0000

    Updated RatGameBuilder to use constructor chaining, and fixed javadoc

--- src/game/RatGameBuilder.java
+++ src/game/RatGameBuilder.java
@@ -2,17 +2,27 @@ package game;
 
 import game.entity.Entity;
 import game.event.GameActionListener;
+import game.generator.ItemGenerator;
 import game.player.Player;
+import game.player.leaderboard.Leaderboard;
 import gui.game.dependant.tilemap.Coordinates;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /**
+ * This class is responsible for building a game from a given level file,
+ * player and event listener. It will construct the necessary dependencies
+ * including the leaderboard, item generator, and entity list.
  *
+ * @author Morgan Gardner
+ * @version 0.1
+ * Copyright: N/A
  */
 public class RatGameBuilder {
+
 
     public RatGameBuilder(File ratGameFile,
                           Player player,
@@ -22,11 +32,12 @@ public class RatGameBuilder {
 
     public RatGameBuilder(File ratGameFile,
                           GameActionListener listener) {
-
+        this(ratGameFile, null, listener);
     }
 
     public RatGame build() {
         //TODO
+        //RatGame game = new RatGame();
         return null;
     }
 
@@ -45,21 +56,26 @@ public class RatGameBuilder {
 
     }
 
+    /**
+     * Fetch the currently active player for the game.
+     * @return active player.
+     */
     private Player loadPlayer() {
         //TODO
         return null;
     }
 
-    /*private Leaderboard loadLeaderboard() {
+    private Leaderboard loadLeaderboard() {
         //TODO
+        //return new Leaderboard(new ArrayList<Player>());
         return null;
     }
-    */
 
-    /*private ItemGenerator loadGenerator() {
+
+    private ItemGenerator loadGenerator() {
         //TODO
         return null;
     }
-    */
+
 
 }
