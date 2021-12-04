3d62eee09874c768070286607100979a7f067254
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2021-12-04 22:24:03 +0000

    End screen uses the embeddable leaderboard

--- src/gui/game/dependant/endscreen/EndScreenController.java
+++ src/gui/game/dependant/endscreen/EndScreenController.java
@@ -1,19 +1,28 @@
 package gui.game.dependant.endscreen;
 
 import game.RatGameProperties;
+import game.contextmap.handler.MovementHandler;
 import game.event.impl.entity.specific.game.GameEndEvent;
 import game.level.reader.RatGameFile;
 import game.level.reader.exception.RatGameFileException;
 import game.player.Player;
 import game.tile.exception.UnknownSpriteEnumeration;
+import gui.leaderboard.split.LeaderboardModule;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -27,75 +36,75 @@ public class EndScreenController {
             = EndScreenController.class.getResource("GameEndScene.fxml");
 
     /**
-     *
+     * Default winner header text.
      */
     private static final String WINNER_HEADER
             = "Congratulations you are a winner!!!";
 
     /**
-     *
+     * Default loser header text.
      */
     private static final String LOSER_HEADER = "Boo you suck!!!";
 
     /**
-     *
+     * Leaderboard vbox which should be loaded with a leaderboard.
+     */
+    @FXML
+    private VBox leaderboardVbox;
+
+    /**
+     * Title of the scene.
      */
     @FXML
     private Label sceneTitleLabel;
 
     /**
-     *
+     * Name of the level.
      */
     @FXML
     private Label levelNameLabel;
 
     /**
-     *
+     * Number of columns that the scene has.
      */
     @FXML
     private Label mapWidthLabel;
 
     /**
-     *
+     * The height of the game map.
      */
     @FXML
     private Label mapHeightLabel;
 
     /**
-     *
+     * The maximum number of rats for the level.
      */
     @FXML
     private Label maxRatsLabel;
 
     /**
-     *
+     * The expected clear time for the level,
      */
     @FXML
     private Label expectedClearTimeLabel;
 
     /**
-     *
+     * The players name.
      */
     @FXML
     private Label playerNameLabel;
 
     /**
-     *
+     * The players clear time.
      */
     @FXML
     private Label playerClearTimeLabel;
 
     /**
-     *
+     * The players total score.
      */
     @FXML
     private Label playerTotalScoreLabel;
-
-    /**
-     *
-     */
-    @FXML
-    private ScrollPane levelLeaderboardScrollPane;
 
     /**
      * @param event The game end event
@@ -153,6 +162,24 @@ public class EndScreenController {
             this.levelNameLabel.setText(
                     file.getDefaultProperties().getLevelName()
             );
+
+            // Load embeddable leaderboard
+            final LeaderboardModule module
+                    = LeaderboardModule.loadAndGet();
+
+            // Todo replace with a call to; file.getLeaderboardPlayers()
+            final List<Player> players = new ArrayList<>();
+            final Random r = new Random();
+            final int bound = 999999;
+            for (int i = 0; i < 50; ++i) {
+                final Player p = new Player("Captain Jack Sparrow");
+                p.setCurrentScore(r.nextInt(bound));
+                p.setPlayTime(r.nextInt(bound));
+                players.add(p);
+            }
+
+            this.leaderboardVbox.getChildren().add(module.getRoot());
+            module.addAllPlayers(players);
 
             // Only an IOException should really occur since we loaded from the
             // RatGameFile anyway
