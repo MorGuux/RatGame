a5a1dbdd9bcdae56ee3c3066538aa0dcc795ece8
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2021-12-05 02:34:03 +0000

    Rat death sound effect.

--- src/game/entity/subclass/rat/Rat.java
+++ src/game/entity/subclass/rat/Rat.java
@@ -10,6 +10,7 @@ import game.entity.Entity;
 import game.entity.subclass.noentry.NoEntry;
 import game.event.impl.entity.specific.general.EntityDeathEvent;
 import game.event.impl.entity.specific.general.EntityMovedEvent;
+import game.event.impl.entity.specific.general.GenericAudioEvent;
 import game.event.impl.entity.specific.general.SpriteChangeEvent;
 import game.level.reader.exception.ImproperlyFormattedArgs;
 import game.level.reader.exception.InvalidArgsContent;
@@ -17,6 +18,7 @@ import game.tile.Tile;
 import game.tile.base.grass.Grass;
 import game.tile.base.path.Path;
 import game.tile.base.tunnel.Tunnel;
+import gui.game.EventAudio.GameAudio;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -699,6 +701,12 @@ public class Rat extends Entity {
 
         if (!taskExecutionService.isShutdown()) {
             this.taskExecutionService.submit(() -> {
+
+                this.fireEvent(new GenericAudioEvent(
+                        this,
+                        GameAudio.RAT_DEATH.getResource()
+                ));
+
                 this.fireEvent(new EntityDeathEvent(
                         this,
                         null,
