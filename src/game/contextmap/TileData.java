5b1eaa7806e6837f6ed5ed84d140c77c977ee0bd
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2021-12-06 09:10:55 +0000

    Update TileData.java
    Override hashcode.

--- src/game/contextmap/TileData.java
+++ src/game/contextmap/TileData.java
@@ -3,6 +3,8 @@ package game.contextmap;
 import game.entity.Entity;
 import game.tile.Tile;
 
+import java.util.Objects;
+
 /**
  * Wrapper class designed to give controlled access to the entities existing
  * on a Tile. Things such as the {@link Tile} and the {@link Entity} that are
@@ -69,6 +71,14 @@ public class TileData {
     }
 
     /**
+     * @return Hashcode of this node based on its internal data.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(node.getRow(), node.getCol(), node.getTile());
+    }
+
+    /**
      * Checks if TileData is equal to an object passed.
      * @param obj object to be checked
      * @return true if obj is TileData instance and coordinates are the same,
@@ -78,7 +88,8 @@ public class TileData {
     public boolean equals(final Object obj) {
         if (obj instanceof TileData x) {
 
-            return this.getRow() == x.getRow() && this.getCol() == x.getCol();
+            return (this.getRow() == x.getRow())
+                    && (this.getCol() == x.getCol());
         }
         return false;
     }
