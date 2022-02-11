847c04a150b89304066bdc518c6b77f084461912
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2022-02-11 19:33:32 +0000

    Level Editor class, its empty right now as I don't know where the FXML for the stage is.

---
+++ src/gui/editor/LevelEditor.java
@@ -0,0 +1,19 @@
+package gui.editor;
+
+import game.level.reader.RatGameFile;
+import javafx.stage.Stage;
+
+/**
+ * Java class created on 11/02/2022 for usage in project RatGame-A2.
+ *
+ * @author -Ry
+ */
+public class LevelEditor {
+
+    private RatGameFile fileToEdit;
+
+    public static LevelEditor init(final Stage s,
+                                   final RatGameFile fileToEdit) {
+        return null;
+    }
+}
