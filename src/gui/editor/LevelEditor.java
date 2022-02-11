b9cbeb959d9c60dc3ab5841b667258fd8ad265b7
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2022-02-11 20:13:04 +0000

    Level Editor setting up basic class elements ready for proper implementations.

--- src/gui/editor/LevelEditor.java
+++ src/gui/editor/LevelEditor.java
@@ -1,28 +1,58 @@
 package gui.editor;
 
+import game.generator.RatItemInventory;
 import game.level.reader.RatGameFile;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Java class created on 11/02/2022 for usage in project RatGame-A2.
  *
  * @author -Ry
  */
-public class LevelEditor {
+public class LevelEditor implements Initializable {
 
     private static final URL SCENE_FXML
             = LevelEditor.class.getResource("LevelEditorMain.fxml");
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Class attributes
+    ///////////////////////////////////////////////////////////////////////////
+
     private Parent root;
     private Stage displayStage;
     private RatGameFile fileToEdit;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Scene FXML attributes
+    ///////////////////////////////////////////////////////////////////////////
+
+    @FXML
+    private BorderPane editorTileViewBorderpane;
+    @FXML
+    private HBox tilesHBox;
+    @FXML
+    private BorderPane generalTabBorderpane;
+    @FXML
+    private BorderPane entitiesTabBorderpane;
+    @FXML
+    private BorderPane itemPoolTabBorderpane;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Static constructors
+    ///////////////////////////////////////////////////////////////////////////
 
     public static LevelEditor init(final Stage s,
                                    final RatGameFile fileToEdit) {
@@ -48,8 +78,22 @@ public class LevelEditor {
     }
 
     ///////////////////////////////////////////////////////////////////////////
+    // Scene gets loaded here; all aspects/editor modules
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void initialize(final URL url,
+                           final ResourceBundle bundle) {
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
     // Event handler methods
     ///////////////////////////////////////////////////////////////////////////
+
+    public void onSaveAndQuit() {
+        getDisplayStage().close();
+    }
 
     ///////////////////////////////////////////////////////////////////////////
     // Data collection/get methods
