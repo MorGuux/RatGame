6be896ad47f2bddd59f2399ccf54f8d8523f7bb1
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2022-02-24 19:56:41 +0000

    JavaDoc

--- src/gui/editor/module/tab/items/form/ItemGeneratorForm.java
+++ src/gui/editor/module/tab/items/form/ItemGeneratorForm.java
@@ -24,12 +24,18 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * Java class created on 17/02/2022 for usage in project RatGame-A2.
+ * Java class created on 17/02/2022 for usage in project RatGame-A2. Form
+ * allows the editing and construction of an {@link ItemGenerator}.
  *
  * @author -Ry
+ * @version 0.1
+ * Copyright: N/A
  */
 public class ItemGeneratorForm implements Initializable {
 
+    /**
+     * Scene fxml resource.
+     */
     private static final URL SCENE_FXML
             = ItemGeneratorForm.class.getResource("ItemGeneratorForm.fxml");
 
@@ -37,14 +43,33 @@ public class ItemGeneratorForm implements Initializable {
     // FXML Attributes
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Choice-box that consists of all the target item generators.
+     */
     @FXML
     private ChoiceBox<ItemGeneratorLoader.GeneratorTarget> itemClassChoiceBox;
+
+    /**
+     * Current number of usages text field.
+     */
     @FXML
     private TextField curUsagesText;
+
+    /**
+     * Max usages text field.
+     */
     @FXML
     private TextField maxUsagesText;
+
+    /**
+     * Current refresh time text field.
+     */
     @FXML
     private TextField curRefreshTimeText;
+
+    /**
+     * Max refresh time text field.
+     */
     @FXML
     private TextField maxRefreshTimeText;
 
@@ -52,14 +77,32 @@ public class ItemGeneratorForm implements Initializable {
     // Class attributes
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Parent/root node of the scene.
+     */
     private Parent root;
+
+    /**
+     * Stage that this form will be displayed on.
+     */
     private Stage displayStage;
+
+    /**
+     * Has the form terminated naturally in that the user has clicked the
+     * button to initiate the termination process.
+     */
     private boolean isNaturalExit = false;
 
     ///////////////////////////////////////////////////////////////////////////
     // Static construction mechanisms
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Static construction mechanism that loads this form into the target stage.
+     *
+     * @param s The stage to load into.
+     * @return Newly constructed instance.
+     */
     public static ItemGeneratorForm init(final Stage s) {
         final FXMLLoader loader = new FXMLLoader(SCENE_FXML);
 
@@ -83,6 +126,10 @@ public class ItemGeneratorForm implements Initializable {
     // Event handlers
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Completes the scene if the scene is safe to complete. Closing the
+     * window that the scene is being displayed in.
+     */
     @FXML
     private void onBuildGeneratorClicked() {
         if (this.isComplete()) {
@@ -99,6 +146,12 @@ public class ItemGeneratorForm implements Initializable {
         }
     }
 
+    /**
+     * Initialises the default values into the scene.
+     *
+     * @param url    Unused.
+     * @param bundle Unused.
+     */
     @Override
     public void initialize(final URL url,
                            final ResourceBundle bundle) {
@@ -118,6 +171,11 @@ public class ItemGeneratorForm implements Initializable {
         this.maxUsagesText.setTextFormatter(v.get());
     }
 
+    /**
+     * Sets the target item class to the provided item.
+     *
+     * @param clazz The target item class.
+     */
     public void setItemClass(final Class<? extends Item> clazz) {
         for (ItemGeneratorLoader.GeneratorTarget t
                 : ItemGeneratorLoader.GeneratorTarget.values()) {
@@ -127,18 +185,38 @@ public class ItemGeneratorForm implements Initializable {
         }
     }
 
+    /**
+     * Set the current number of usages text to the provided usages.
+     *
+     * @param cur Current number of usages.
+     */
     public void setCurUsagesText(final int cur) {
         this.curUsagesText.setText(String.valueOf(cur));
     }
 
+    /**
+     * Set the maximum number of usages text.
+     *
+     * @param max The maximum number of usages.
+     */
     public void setMaxUsagesText(final int max) {
         this.maxUsagesText.setText(String.valueOf(max));
     }
 
+    /**
+     * Set the current refresh time text.
+     *
+     * @param cur The current refresh time.
+     */
     public void setCurRefreshTimeText(final int cur) {
         this.curRefreshTimeText.setText(String.valueOf(cur));
     }
 
+    /**
+     * Set the maximum refresh time text.
+     *
+     * @param max The maximum refresh time.
+     */
     public void setMaxRefreshTimeText(final int max) {
         this.maxRefreshTimeText.setText(String.valueOf(max));
     }
@@ -147,6 +225,13 @@ public class ItemGeneratorForm implements Initializable {
     // Data collection methods (get/set)
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Checks to see if the current state of the form is complete in that all
+     * data fields are parseable.
+     *
+     * @return {@code true} if it is possible to parse all the data fields
+     * into the target types.
+     */
     public boolean isComplete() {
         // Check data validity
         final Function<TextField, Boolean> fn = (t) -> !t.getText().equals("");
@@ -157,18 +242,35 @@ public class ItemGeneratorForm implements Initializable {
                 && fn.apply(maxRefreshTimeText);
     }
 
+    /**
+     * @return Root node of the scene.
+     */
     public Parent getRoot() {
         return root;
     }
 
+    /**
+     * @return Stage that this form is being displayed in.
+     */
     public Stage getDisplayStage() {
         return displayStage;
     }
 
+    /**
+     * @return {@code true} if the form has terminated naturally.
+     */
     public boolean isNaturalExit() {
         return isNaturalExit;
     }
 
+    /**
+     * Parses all the data fields and builds it into the target Generator type.
+     *
+     * @return Optional which can contain a new instance of an Item generator
+     * using the data fields as the build parameters. However, if the form is
+     * not complete or if some error occurs whilst parsing the data then an
+     * empty optional is returned instead.
+     */
     public Optional<ItemGenerator<?>> createGenerator() {
 
         // We only allow positive integers
@@ -208,6 +310,9 @@ public class ItemGeneratorForm implements Initializable {
         }
     }
 
+    /**
+     * Clears this form so that it can be reused.
+     */
     public void clear() {
         this.isNaturalExit = false;
         this.maxRefreshTimeText.clear();
